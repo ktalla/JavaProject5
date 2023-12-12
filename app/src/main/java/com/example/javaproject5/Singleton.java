585f6a52eaeb3952bb2a044e23404e5f14fad153
Ishani Mhatre <93501009+ishanimhatre@users.noreply.github.com>
2023-12-11 21:54:38 -0500

    Update Singleton.java

--- app/src/main/java/com/example/javaproject5/Singleton.java
+++ app/src/main/java/com/example/javaproject5/Singleton.java
@@ -3,16 +3,32 @@ package com.example.javaproject5;
 
 import java.util.ArrayList;
 
+/**
+ * Singleton class to create one instance of pizza object, error handling
+ * @author Keerthana Talla
+ */
     public class Singleton {
-        private static final Singleton instance = new Singleton();
+    /**
+     * public constructor
+     */
+    private static final Singleton instance = new Singleton();
 
         private Order order;
         private StoreOrders storeOrders;
 
-        public Pizza getPizza() {
+    /**
+     * getter method for pizza object
+     * @return pizza object
+     */
+
+    public Pizza getPizza() {
             return pizza;
         }
 
+    /**
+     * setter method for pizza object
+     * @return pizza object
+     */
         public void setPizza(Pizza pizza) {
             this.pizza = pizza;
         }
@@ -21,43 +37,87 @@ import java.util.ArrayList;
 
         private boolean orderAdded = false;
 
+    /**
+     * getter instance method
+     * @return current instance of pizza
+     */
+
         public static Singleton getInstance(){
             return instance;
         }
 
+
+    /**
+     * method to arrange items in order
+     * @param pizzaList array list of pizzas ordered
+     */
         public void initializeOrder(ArrayList<Pizza> pizzaList){
             order = new Order(pizzaList);
         }
 
+    /**
+     * getter method for current order
+     * @return order
+     */
         public Order getOrder() {
             return order;
         }
 
+
+    /**
+     * getter method for order added
+     * @return order added
+     */
         public boolean getOrderAdded(){
             return orderAdded;
         }
 
+
+    /**
+     * setter method for order added
+     * @param bool true or false
+     */
         public void setOrderAdded(boolean bool){
             orderAdded = bool;
         }
 
+    /**
+     * setter method for order
+     * @param newOrder  order
+     */
         public void setOrder(Order newOrder){
             order = newOrder;
         }
 
+    /**
+     * initialize array list with store orders
+     * @param ordersList of pizza items
+     */
         public void initializeStoreOrders(ArrayList <Order> ordersList){
             storeOrders = new StoreOrders(ordersList);
         }
 
-        public StoreOrders getStoreOrders(){
+    /**
+     * getter method for store orders
+     * @return store orders
+     */
+    public StoreOrders getStoreOrders(){
             return storeOrders;
         }
 
-        public void setStoreOrders(StoreOrders newStoreOrder){
+    /**
+     * setter method for store orders
+     * @param newStoreOrder store orders
+     */
+    public void setStoreOrders(StoreOrders newStoreOrder){
             storeOrders = newStoreOrder;
         }
 
-        public int getCurrentOrderNum(){
+    /**
+     * getter method for current order number, prevents duplicate instances/orders
+     * @return order number
+     */
+    public int getCurrentOrderNum(){
             if(storeOrders!=null){
                 if(storeOrders.getOrders()!=null)
                     return StoreOrders.getNextOrderNumber();
